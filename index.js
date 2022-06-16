318f63f3723aec1541afd11427f69b8b0645c94f
fatima ezzahraa el <fatimzahraelmadini@gmail.com>
2022-06-16 12:08:29 +0200

    change port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv'
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
 
 dotenv.config()
 const uri = "mongodb+srv://fatimzahra:anahna1993@cluster0.etmbixq.mongodb.net/?retryWrites=true&w=majority";
