9c23fb9b84aa0398323f9c6a86489c22c45ec864
fatima ezzahraa el <fatimzahraelmadini@gmail.com>
2022-06-16 11:16:01 +0200

    Change .env

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import routes from './src/routes/Student.js';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
 
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 3000;
 
-
+dotenv.config()
 const uri = "mongodb+srv://fatimzahra:anahna1993@cluster0.etmbixq.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(uri, {
   useNewUrlParser: true,
@@ -23,4 +24,5 @@ app.get('/', (req, res) =>
 routes(app);
 
 app.listen(PORT, () =>
-console.log(`Your server is running on port ${PORT}`))
+{console.log(`Your server is running on port ${PORT}`)
+})
